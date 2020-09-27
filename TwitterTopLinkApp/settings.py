144dd3a30eec346a44ba55e0524c68294779a87e
Harsh Upadhyay <akshat_nigam@infoobjects.com>
2020-09-27 13:24:27 +0530

    Minor chanegs

--- TwitterTopLinkApp/settings.py
+++ TwitterTopLinkApp/settings.py
@@ -143,5 +143,5 @@ STATICFILES_DIRS = [
 
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))   
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
